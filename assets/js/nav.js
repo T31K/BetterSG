a8cb9ff74d8fe50d935bea009da2a2b743ffe785
T31K <t31kmunwong@gmail.com>
2020-12-03 18:09:06 +0800

    Added skyscrapper animated svg

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,28 +1,11 @@
 console.clear();
 
-const app = (() => {
-  let body;
-  let menu;
-  let menuItems;
-
-  const init = () => {
-    body = document.querySelector('body');
-    menu = document.querySelector('.menu-icon');
-    menuItems = document.querySelectorAll('.nav__list-item');
-
-    applyListeners();
-  };
-
-  const applyListeners = () => {
-    menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
-  };
-
-  const toggleClass = (element, stringClass) => {
-    if (element.classList.contains(stringClass))
-    element.classList.remove(stringClass);else
-
-    element.classList.add(stringClass);
-  };
-
-  init();
-})();
+$(document).ready(function() {
+  $('#navbarDropdown').mouseenter(function() {
+    $('.dropdown-menu').slideToggle(300, "linear");
+  });
+  
+  $('.dropdown-menu').mouseleave(function() {
+    $(this).slideToggle(300, "linear");
+  });
+});
